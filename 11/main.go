057a9ba4c858109268826948773b574f09fb4cc3
Guilherme Stoll <gs.toaldo@gmail.com>
2025-12-12 14:23:25 -0300

    day 11 part 2 - unfinished

--- 11/main.go
+++ 11/main.go
@@ -24,13 +24,13 @@ func parse(path string) G {
 	return input
 }
 
-func countPaths(input G) int {
+func countPaths(input G, start, end string) int {
 	visited := map[string]bool{}
 
 	var dfs func(curr string) int
 
 	dfs = func(curr string) int {
-		if curr == "out" {
+		if curr == end {
 			return 1
 		}
 
@@ -49,10 +49,57 @@ func countPaths(input G) int {
 		return count
 	}
 
-	return dfs("you")
+	return dfs(start)
+}
+
+func blocklist(input G) map[string]bool {
+	result := map[string]bool{}
+	for origin := range input {
+		if countPaths(input, origin, "fft") == 0 && countPaths(input, origin, "dac") == 0 {
+			result[origin] = true
+		}
+	}
+	return result
+}
+
+func countPathsP2(input G, blocklist map[string]bool) int {
+	visited := map[string]bool{}
+
+	var dfs func(curr string) int
+
+	dfs = func(curr string) int {
+		if curr == "out" {
+			if visited["dac"] && visited["fft"] {
+				return 1
+			}
+
+			return 0
+		}
+
+		count := 0
+
+		for _, n := range input[curr] {
+			if visited[n] {
+				continue
+			}
+
+			if blocklist[n] && !visited["fft"] && !visited["dac"] {
+				continue
+			}
+
+			visited[n] = true
+			count += dfs(n)
+			visited[n] = false
+		}
+
+		return count
+	}
+
+	return dfs("svr")
 }
 
 func main() {
 	input := parse(utils.FilePath())
-	fmt.Println("p1:", countPaths(input))
+	fmt.Println("p1:", countPaths(input, "you", "out"))
+	fmt.Println("p2:", countPathsP2(input, blocklist(input)))
 }
